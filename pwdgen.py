9722471ecb4f8813bfcb6bc9721588497bc3fe28
GeorgeNLQ <70286427+GeorgeTorres-py@users.noreply.github.com>
2020-09-12 13:50:11 -0700

    Update pwdgen.py

--- pwdgen.py
+++ pwdgen.py
@@ -1,5 +1,8 @@
 import string
 from random import *
+
 characters = string.ascii_letters + string.punctuation  + string.digits
+
 password =  "".join(choice(characters) for x in range(randint(10, 20)))
+
 print(password)
